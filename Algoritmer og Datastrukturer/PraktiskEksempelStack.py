6f0ee57ebfe40ec0180ed6417a98f4f0d8dd5761
Sanlys <49021092+Sanlys@users.noreply.github.com>
2022-01-21 14:00:35 +0100

    Implementert stack, ikke ferdig med bruk

---
+++ Algoritmer og Datastrukturer/PraktiskEksempelStack.py
@@ -0,0 +1,19 @@
+stack = ["wow", "stack", "1", "2", "haha"]
+
+stack.append("Tekst")
+stack.append("Mer tekst")
+stack.append("Enda mer tekst")
+
+print(stack)
+
+stack.pop()
+stack.pop()
+
+print(stack)
+
+
+#bruk av stack
+
+stack = []
+
+stack.append
